b9e82621cc91538d89196615d9d191f3a6c18c40
Mayank1094 <mayankkamble1094@gmail.com>
2026-03-10 14:23:27 +0530

    Update Geometric shape.c

--- Geometric shape.c
+++ Geometric shape.c
@@ -7,7 +7,7 @@ int main() {
     initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
 
     line(50, 50, 200, 50);
-    rectangle(150, 80, 200, 160);
+    rectangle(50, 80, 200, 160);
     circle(350, 120, 50);
     ellipse(350, 250, 0, 360, 80, 40);
     line(100, 250, 50, 330);
